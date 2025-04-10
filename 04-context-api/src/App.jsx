2bed854996a4642a0f5223447dc53b5e1cb3207d
Ahsan Sajjad <ahsan.sajjad@swyftlogic.org>
2025-04-10 15:41:23 +0500

    ..

---
+++ 04-context-api/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Form from './components/Form'
+import TodoList from './components/TodoList'
+
+function App() {
+  
+
+  return (
+   <div>
+    
+    <h1>Todo App</h1>
+   <Form/>
+   <TodoList/>
+
+
+
+
+   </div>
+  )
+}
+
+export default App
